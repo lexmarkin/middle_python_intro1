670d3396b48563ebafc68b79a38d840c6c2fc48b
lex <aleksandr_07.ru@mail.ru>
2022-07-21 18:09:35 +0300

    Good Style Code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
-"""Генератор приветствий."""
-import pprint
+# """Генератор приветствий."""
 
-def Greeting(name: str) -> str:
-      """Возвращает текст приветствия.
 
-      Args:
-          name: Имя пользователя
-
-      Returns:
-          int: Текст приветствия
-      """
-      # pprint.pprint(name.lower())
-      return 'Привет, name'
+def greeting(name: str) -> str:
+    # """Возвращает текст приветствия.
+    #
+    # Args:
+    #     name: Имя пользователя
+    #
+    # Returns:
+    #     int: Текст приветствия
+    # """
+    capitalize_name = ' '.join([arg.capitalize() for arg in name.split()])
+    return ''.join(['Привет, ', capitalize_name])
